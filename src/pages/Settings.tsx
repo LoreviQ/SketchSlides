0a7732bad20c5dc5aa2fefa5e644d2ffd207276a
LoreviQ <lorevi@protonmail.com>
2024-11-24 18:44:39 +0000

    Adjusted narrow padding on ScheduleDetails

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -403,7 +403,7 @@ function ScheduleDetails({ schedules, selectedSchedule, updateSchedules, onSave 
         setTempSchedule(selectedSchedule);
     }, [selectedSchedule]);
     return (
-        <div className="pl-4 space-y-4">
+        <div className={`space-y-4`}>
             <div className="flex justify-center">
                 <input
                     value={tempSchedule.title}
