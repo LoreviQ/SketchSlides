93685a6088d0f4250edf78bb885d2761327471d1
LoreviQ <lorevi@protonmail.com>
2024-11-24 19:08:17 +0000

    Extracted hero to components

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -14,6 +14,7 @@ import { formatFileSize } from "../utils/formatters";
 import { saveLastFolder, getLastFolder } from "../utils/indexDB";
 import { sessionTypeToDescription } from "../utils/session";
 import { useApp } from "../contexts/AppContext";
+import { Hero } from "../components/style";
 
 export default function Settings({}) {
     const { preferences } = usePreferences();
@@ -89,23 +90,7 @@ export default function Settings({}) {
     return (
         <div className="w-screen flex justify-center px-6">
             <div className="w-full h-screen max-w-2xl p-6 flex flex-col space-y-4">
-                <div className="relative aspect-[3/1] max-h-48 overflow-hidden">
-                    <div className="absolute inset-0 flex items-start">
-                        <img
-                            src="/icons/framerate.png"
-                            alt="FrameRate Logo"
-                            className="w-full object-cover opacity-50"
-                        />
-                    </div>
-                    <div className="absolute inset-0 bg-gradient-to-b from-custom-dark from-0% via-transparent to-transparent" />
-                    <div className="absolute inset-0 bg-gradient-to-l from-custom-dark from-0% via-transparent to-transparent" />
-                    <div className="absolute inset-0 bg-gradient-to-r from-custom-dark from-0% via-transparent to-transparent" />
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <h1 className="pt-12 text-5xl sm:text-7xl font-bold text-center dark:text-white z-10 font-kalam text-stroke-1 text-stroke-black">
-                            FrameRate
-                        </h1>
-                    </div>
-                </div>
+                <Hero />
                 <ActionButton onClick={handleFolderSelect} label="Select Folder" colour="bg-blue-600" />
                 <FolderDetails selectedFolder={selectedFolder} />
                 <hr className="border-gray-300 dark:border-gray-700" />
