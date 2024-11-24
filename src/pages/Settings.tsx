8a904a8c5607d6643c7695d3b2ac19f2f72ed370
LoreviQ <lorevi@protonmail.com>
2024-11-24 19:07:04 +0000

    Adjusted hero look

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -89,7 +89,7 @@ export default function Settings({}) {
     return (
         <div className="w-screen flex justify-center px-6">
             <div className="w-full h-screen max-w-2xl p-6 flex flex-col space-y-4">
-                <div className="relative h-48 overflow-hidden">
+                <div className="relative aspect-[3/1] max-h-48 overflow-hidden">
                     <div className="absolute inset-0 flex items-start">
                         <img
                             src="/icons/framerate.png"
@@ -97,8 +97,11 @@ export default function Settings({}) {
                             className="w-full object-cover opacity-50"
                         />
                     </div>
+                    <div className="absolute inset-0 bg-gradient-to-b from-custom-dark from-0% via-transparent to-transparent" />
+                    <div className="absolute inset-0 bg-gradient-to-l from-custom-dark from-0% via-transparent to-transparent" />
+                    <div className="absolute inset-0 bg-gradient-to-r from-custom-dark from-0% via-transparent to-transparent" />
                     <div className="absolute inset-0 flex items-center justify-center">
-                        <h1 className="pt-4 text-5xl sm:text-7xl font-bold text-center dark:text-white z-10 font-kalam text-stroke-1 text-stroke-black">
+                        <h1 className="pt-12 text-5xl sm:text-7xl font-bold text-center dark:text-white z-10 font-kalam text-stroke-1 text-stroke-black">
                             FrameRate
                         </h1>
                     </div>
